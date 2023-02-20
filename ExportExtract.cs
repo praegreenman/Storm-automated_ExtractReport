0390e4abc2d93a0859ba7fb53c800a74006b9d5a
praegreenman <pare@wrmsoftware.com>
2023-02-20 11:13:47 +0700

    fix error

--- ExportExtract.cs
+++ ExportExtract.cs
@@ -59,7 +59,7 @@ namespace ExtractReport
             SelectEvent.Click();
             Thread.Sleep(2000);
 
-            var selectEleEvent = webDriver.FindElement(By.Id("ASPxPanel2_ContentPlaceHolder1_gluEvents_DDD_gv_tccell1_0"));
+            var selectEleEvent = webDriver.FindElement(By.Id("ASPxPanel2_ContentPlaceHolder1_gluEvents_DDD_gv_tccell3_0"));
             Thread.Sleep(2000);
             selectEleEvent.Click();
             Thread.Sleep(2000);
@@ -82,31 +82,27 @@ namespace ExtractReport
 
         public void CustomizedReportExtract()
         {
-            //dropdown select report 
-            IWebElement Report = webDriver.FindElement(By.Id("ASPxPanel2_ContentPlaceHolder1_cpnMain_cbReport_B-1"));
-            Report.Click();
-            Thread.Sleep(2000);
-
-            //select workforce :
-            IWebElement ListWorkforce = webDriver.FindElement(By.Id("ASPxPanel2_ContentPlaceHolder1_cpnMain_cbReport_DDD_L_LBI1T0"));
-            ListWorkforce.Click();
-            Thread.Sleep(5000);
-
-
+   
             //input Description
             var Desinput = webDriver.FindElement(By.Id("ASPxPanel2_ContentPlaceHolder1_cpnMain_mmDescription_I"));
             Desinput.SendKeys("TestAutomatedByPare");
             Thread.Sleep(2000);
 
             //dropdown select data source
-            IWebElement DataSource = webDriver.FindElement(By.Id("ASPxPanel2_ContentPlaceHolder1_cpnMain_cbDataSource_B-1"));
+            IWebElement DataSource = webDriver.FindElement(By.Name("ctl00$ASPxPanel2$ContentPlaceHolder1$cpnMain$cbDataSource"));
             DataSource.Click();
             Thread.Sleep(2000);
 
-            //select lodging
-            IWebElement LodgingData = webDriver.FindElement(By.Id("ASPxPanel2_ContentPlaceHolder1_cpnMain_cbDataSource_DDD_L_LBI2T0"));
+            //select Timesheet
+            IWebElement LodgingData = webDriver.FindElement(By.Id("ASPxPanel2_ContentPlaceHolder1_cpnMain_cbDataSource_DDD_L_LBI0T0"));
             LodgingData.Click();
             Thread.Sleep(2000);
+
+            // INPUT DATE
+            IWebElement DateStart = webDriver.FindElement(By.Id("ASPxPanel2_ContentPlaceHolder1_cpnMain_deStartDate_I"));
+            DateStart.SendKeys("02/13/2023");
+            IWebElement DateEnd = webDriver.FindElement(By.Id("ASPxPanel2_ContentPlaceHolder1_cpnMain_deEndDate_I"));
+            DateEnd.SendKeys("02/18/2023");
 
 
         }
@@ -114,53 +110,14 @@ namespace ExtractReport
 
         public void checkboxChoose(){
 
-            //check box 
-            IWebElement AssignLocation = webDriver.FindElement(By.XPath("(.//*[normalize-space(text()) and normalize-space(.)='Assigned_Location'])[1]/preceding::span[2]"));
-            AssignLocation.Click();
-            Thread.Sleep(1000);
-            IWebElement Crewsheet = webDriver.FindElement(By.XPath("(.//*[normalize-space(text()) and normalize-space(.)='Crew_Sheet'])[1]/preceding::span[2]"));
-            Crewsheet.Click();
-            Thread.Sleep(1000);
-            IWebElement CrewsheetName = webDriver.FindElement(By.XPath("(.//*[normalize-space(text()) and normalize-space(.)='Crew_Sheet_Name'])[1]/preceding::span[2]"));
-            CrewsheetName.Click();
-            Thread.Sleep(2000);
-
-            IWebElement HotelName = webDriver.FindElement(By.XPath(".//*/text()[normalize-space(.)='Lodging_Assigned_Date']/parent::*"));
-            HotelName.Click();
-            Thread.Sleep(1000);
-
-            IWebElement LodgingDate = webDriver.FindElement(By.XPath(".//*/text()[normalize-space(.)='Operating_Company']/parent::*"));
-            LodgingDate.Click();
-            Thread.Sleep(1000);
-
-            IWebElement OC = webDriver.FindElement(By.XPath(".//*/text()[normalize-space(.)='Operating_Company']/parent::*"));
-            OC.Click();
-            Thread.Sleep(1000);
-            IWebElement region = webDriver.FindElement(By.XPath(".//*/text()[normalize-space(.)='Region']/parent::*"));
-            region.Click();
-            Thread.Sleep(2000);
-
             //add field
-            IWebElement AddButton = webDriver.FindElement(By.CssSelector("#ASPxPanel2_ContentPlaceHolder1_cpnMain_cpnDataSource_btnMoveSelectedItemsToRight_CD"));
-            AddButton.Click();
+            IWebElement AddAllButton = webDriver.FindElement(By.CssSelector("#ASPxPanel2_ContentPlaceHolder1_cpnMain_cpnDataSource_btnMoveAllItemsToRight_CD"));
+            AddAllButton.Click();
             Thread.Sleep(2000);
             
-
-            // save Report
-            IWebElement SaveButton = webDriver.FindElement(By.CssSelector("#ASPxPanel2_ContentPlaceHolder1_cpnMain_btnSave_CD"));
-            SaveButton.Click();
-            Thread.Sleep(2000);
-
-            //edit reportname
-            var NameReport = "paretestt";
-            var NameReportinput = webDriver.FindElement(By.Id("ASPxPanel2_ContentPlaceHolder1_cpnMain_pucReportName_tbPucReportName_I"));
-            NameReportinput.SendKeys(NameReport);
-
-            
-
-            // comfirm save 
-            IWebElement SaveConfirmButton = webDriver.FindElement(By.CssSelector("#ASPxPanel2_ContentPlaceHolder1_cpnMain_pucReportName_btnSavePucReportName_CD"));
-            SaveConfirmButton.Click();
+            // Retrieve Data
+            IWebElement RetrieveData = webDriver.FindElement(By.CssSelector("#ASPxPanel2_ContentPlaceHolder1_cpnMain_btnRefresh"));
+            RetrieveData.Click();
             Thread.Sleep(5000);
             
         }
@@ -175,6 +132,7 @@ namespace ExtractReport
             //csv
             IWebElement ExportCSV = webDriver.FindElement(By.Id("ASPxPanel2_ContentPlaceHolder1_cpnMain_popUpExport_mnExport_DXI2_T"));
             ExportCSV.Click();
+            Thread.Sleep(2000);
             
 
 
